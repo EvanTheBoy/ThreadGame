1c8dd9efa1d513142d2c34a97478a90f27154904
Evan <761356554@qq.com>
2022-05-08 20:37:33 +0800

    Update GameUI.java

--- src/com/hw/frame/GameUI.java
+++ src/com/hw/frame/GameUI.java
@@ -7,7 +7,9 @@ public class GameUI extends JFrame {
     public Graphics g;
     public void showFrame() {
         this.setSize(1200, 1200);
-
+        this.setLayout(new FlowLayout());
+        this.setDefaultCloseOperation(3);
+        this.setLocationRelativeTo(null);
         this.setVisible(true);
     }
 
