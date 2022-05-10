088aeca76cad8422ed2014b324f2e9078f0bdd67
Evan <761356554@qq.com>
2022-05-10 21:46:05 +0800

    Update GameThread.java

--- src/com/hw/thread/GameThread.java
+++ src/com/hw/thread/GameThread.java
@@ -1,6 +1,10 @@
 package com.hw.thread;
 
+import java.awt.*;
+
 public class GameThread implements Runnable {
+    public Graphics g;
+    private boolean gameRest; //判断游戏是否暂停
     @Override
     public void run() {
 
