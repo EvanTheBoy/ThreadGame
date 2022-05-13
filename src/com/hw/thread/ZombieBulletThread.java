acdfe4ebec0c5937d6a2b811fceedd5f8a07e00f
Evan <761356554@qq.com>
2022-05-13 22:03:06 +0800

    Update ZombieBulletThread.java

--- src/com/hw/thread/ZombieBulletThread.java
+++ src/com/hw/thread/ZombieBulletThread.java
@@ -1,6 +1,10 @@
 package com.hw.thread;
 
 public class ZombieBulletThread implements Runnable {
+    public int count;
+    public ZombieBulletThread() {
+        count = 0;
+    }
     @Override
     public void run() {
 
